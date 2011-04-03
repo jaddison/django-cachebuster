8879e0c5df3c4779b5d9da88c9aff38db8a5adda
James Addison <code@scottisheyes.com>
2011-04-03 13:10:06 -0700

    updating the version so pypi can be updated (only documentation updates)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='James Addison',
     author_email='code@scottisheyes.com',
     packages = ['cachebuster','cachebuster.templatetags','cachebuster.detectors'],
-    version = '0.2',
+    version = '0.2.1',
     url='http://github.com/jaddison/django-cachebuster',
     keywords=[],
     license='BSD',
